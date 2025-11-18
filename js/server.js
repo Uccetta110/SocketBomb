9c7f875996345cec56dcd1186a4e23685056d90b
AGLIATI FEDERICO <AGLIATI.21076>
2025-11-18 11:12:33 +0100

    primo caricamento

---
+++ js/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
